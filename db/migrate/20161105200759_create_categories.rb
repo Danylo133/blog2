a48167e64a0ec1b8a73147c0fafa6cd4ace4bd50
Danylo133 <danylomandryk@gmail.com>
2016-11-06 12:05:04 +0200

    add comments to post

---
+++ db/migrate/20161105200759_create_categories.rb
@@ -0,0 +1,18 @@
+class CreateCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :categories do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    Category.create :name => "History"
+    Category.create :name => "Math"
+    Category.create :name => "Psychology"
+    Category.create :name => "Lovey"
+
+
+  end
+
+
+
+end
