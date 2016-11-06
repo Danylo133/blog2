5607a7758c6786f3385aa3e90bfc369b64b2a199
Danylo133 <danylomandryk@gmail.com>
2016-11-07 00:52:12 +0200

    extract getting all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
   @posts = Post.all
@@ -24,17 +23,13 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
     @categories = Category.all
   end
 
   def show
-    @post =Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -44,7 +39,6 @@ class PostsController < ApplicationController
     end
 
   def destroy
-   @post = Post.find(params[:id])
    @post.destroy
 
    redirect_to posts_path
@@ -60,6 +54,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :text, :category_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
