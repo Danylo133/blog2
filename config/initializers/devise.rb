558d824672c56e48bd712235645edb3432621ddc
Danylo133 <danylomandryk@gmail.com>
2016-11-07 21:57:43 +0200

    add login by facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -247,7 +247,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+   config.omniauth :facebook, '708096899340000', '1765ab3a5ccc8667060d1ce983fe86b4', callback_url: 'http://localhost:3000/users/auth/facebook/callback' #, scope: 'user,public_repo'
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
