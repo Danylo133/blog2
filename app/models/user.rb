463611032b6a19bfb3fe02b27a493cc2da716e91
Danylo133 <danylomandryk@gmail.com>
2016-11-13 20:06:52 +0200

    add field name for user and add field name in Sign_up. made validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  validates :name, presence: true
+
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      # user.name = auth.info.name   # assuming the user model has a name
+      user.name = auth.info.name   # assuming the user model has a name
       #  user.image = auth.info.image # assuming the user model has an image
     end
   end
