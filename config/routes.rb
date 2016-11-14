46b3e9d8d3ab030402f435742a0dc63ec7fcd1cb
Danylo133 <danylomandryk@gmail.com>
2016-11-14 14:21:21 +0200

    show all posts from category

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'categories/posts'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
 
   get 'users/:id/posts' => 'users#posts', :as => :user_posts
+  get 'categories/:id/posts' => 'categories#posts', :as => :category_posts
+  #resources :categories do
+ #   resources :posts
+ # end
+
 
   root 'posts#index'
 
