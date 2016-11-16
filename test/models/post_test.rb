1f4a5dd68725e4c3837b40d1d022f0a8242860fa
Danylo133 <danylomandryk@gmail.com>
2016-11-16 21:57:17 +0200

    add tests for post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post with title less 5" do
+   post = Post.new
+   post.title = "sd"
+   post.category = Category.all[0]
+   post.text = "dfgdfgdfgdfg"
+   assert_not post.save, "Saved the article without a title"
+  end
+
+  test "should not save without category" do
+      post = Post.new
+  post.title = "ddddddd"
+    post.text = "jdjdjbj"
+    assert_not post.save
+  end
 end
