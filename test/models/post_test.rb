ee928c78568e51e5896ecdbec530ae8fe98612a2
Danylo133 <danylomandryk@gmail.com>
2016-11-17 11:07:53 +0200

    add test "should not save without text" to post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,17 +2,24 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   test "should not save post with title less 5" do
-   post = Post.new
-   post.title = "sd"
-   post.category = Category.all[0]
-   post.text = "dfgdfgdfgdfg"
-   assert_not post.save, "Saved the article without a title"
+    post = Post.new
+    post.title = "FT"
+    post.category = Category.all[0]
+    post.text = "Fake text"
+    assert_not post.save, "Saved the article without a title"
   end
 
   test "should not save without category" do
-      post = Post.new
-  post.title = "ddddddd"
-    post.text = "jdjdjbj"
+    post = Post.new
+    post.title = "Fake Title"
+    post.text = "Fake text"
+    assert_not post.save
+  end
+
+  test "should not save without text" do
+    post =Post.new
+    post.title = "Fake Title"
+    post.category = Category.all[0]
     assert_not post.save
   end
 end
