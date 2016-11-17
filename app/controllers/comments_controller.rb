3cf7c2030b2167d9cbbb3a576fb5079263723fa4
Danylo133 <danylomandryk@gmail.com>
2016-11-17 14:51:27 +0200

    reformat code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
+  before_action :set_comments, only: [:edit, :update, :destroy]
+
+
+  def new
+    @comment = Comment.new
+  end
 
   def create
     @post = Post.find(params[:post_id])
@@ -13,13 +19,9 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     if @comment.update(comment_params)
       redirect_to post_path(@post)
     end
@@ -27,8 +29,6 @@ class CommentsController < ApplicationController
 
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
 
     redirect_to post_path(@post)
@@ -37,6 +37,11 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body, :id, :post_id)
+  end
+
+  def set_comments
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
   end
 end
 
