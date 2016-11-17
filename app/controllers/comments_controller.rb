221e91057e95477e6ca2e2f9802d227ed07036b2
Danylo133 <danylomandryk@gmail.com>
2016-11-17 14:19:55 +0200

    made that user name appropriate  to commenter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     @comment.user = current_user
+    @comment.commenter = current_user.name
     @comment.save
     redirect_to post_path(@post)
   end
